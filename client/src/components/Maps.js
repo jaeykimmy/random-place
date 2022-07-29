fcbae4a76a304efde436ce695d09056824ece3e0
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-07-29 14:30:24 -0400

    mui add, rating and # of reviews

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { TextField, Rating } from "@mui/material";
 
 export default function Maps() {
   const [data, setData] = useState([]);
@@ -8,6 +9,10 @@ export default function Maps() {
   const handleChange = (event) => {
     setData(event.target.value);
   };
+  // axios.post("http://localhost:4000/", {
+  //   query: e.target.value,
+  // });
+
   useEffect(() => {
     axios("http://localhost:4000/")
       .then(function (response) {
@@ -21,14 +26,26 @@ export default function Maps() {
   const goodCoffee = data.filter((x) => {
     return x.rating > 4.4;
   });
+  console.log(goodCoffee);
 
   return (
     <div>
+      <TextField onChange={(e) => console.log(e.target.value)} />
       {goodCoffee.map((x) => {
         return (
           <div key={x.place_id}>
             <h2>{x.name}</h2>
-            <p>{x.rating}</p>
+            <p>
+              Rating: {x.rating}
+              <Rating
+                name="read-only"
+                value={x.rating}
+                precision={0.1}
+                readOnly
+              />
+            </p>
+
+            <p>{x.user_ratings_total} Reviews</p>
           </div>
         );
       })}

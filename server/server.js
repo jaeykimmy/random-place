c3e785773e319c3343299ef45c02c79a70ad9771
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-07-31 20:54:44 -0400

    post req on server makes sense

--- server/server.js
+++ server/server.js
@@ -40,19 +40,8 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.query);
-  axios
-    .get(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee+shop&location=${lat},${lng}&radius=1000&region=us&type=cafe,bakery&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
-    )
-    .then(function (response) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-      res.send(response.data);
-      // console.log(response.data.results);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.send(response.data);
 });
 
 app.listen(4000);
